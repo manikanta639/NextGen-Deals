eb7e6a525c00411bc40085e85e6dd3159634508d
manikanta639 <manikantanagubandi9@gmail.com>
2025-04-17 22:21:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const products = [
     {
         image: "https://d3lopvpsghm5w.cloudfront.net/Screenshot%202025-03-29%20at%206.28.58%E2%80%AFPM.webp?w=100&h=100&fit=crop&auto=format",
         link: "https://amzn.to/3E0U9aU",
-        number: "#599"
+        number: "#589"
     },
 ];
 
