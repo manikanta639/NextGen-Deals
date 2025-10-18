11e72a29857520141c0b161cdf515a3982936fbb
manikanta639 <manikantanagubandi9@gmail.com>
2025-10-18 16:12:47 +0530

    Add files via upload

---
+++ service-worker (1).js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "campusbuys-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192x192.png",
+  "/icon-512x512.png"
+  // Add other assets such as CSS, JS, images as needed
+];
+
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installed");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activated");
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log("Deleting old cache:", cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
