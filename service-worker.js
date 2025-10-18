a48954019ba2a0bb27ff12003ec3f293a7a45f01
manikanta639 <manikantanagubandi9@gmail.com>
2025-10-18 16:55:35 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,39 +4,47 @@ const urlsToCache = [
   "/index.html",
   "/manifest.json",
   "/icon-192x192.png",
-  "/icon-512x512.png"
+  "/icon-512x512.png",
   // Add other assets such as CSS, JS, images as needed
 ];
 
 self.addEventListener("install", (event) => {
   console.log("Service Worker installed");
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    }).then(() => self.skipWaiting())
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting()) // Activate worker immediately after install
   );
 });
 
 self.addEventListener("activate", (event) => {
   console.log("Service Worker activated");
   event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log("Deleting old cache:", cacheName);
-            return caches.delete(cacheName);
-          }
-        })
+    caches.keys() // Get all cache keys
+      .then((cacheNames) =>
+        Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheName !== CACHE_NAME) {
+              console.log("Deleting old cache:", cacheName);
+              return caches.delete(cacheName);
+            }
+            return null;
+          })
+        )
       )
-    ).then(() => self.clients.claim())
+      .then(() => self.clients.claim()) // Take control of uncontrolled clients
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      if (response) {
+        return response; // Return cached version
+      }
+      return fetch(event.request).catch(() => {
+        // Optionally, return an offline fallback here for navigation requests.
+      });
     })
   );
 });
